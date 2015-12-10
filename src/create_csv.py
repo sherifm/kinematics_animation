7c16ad99f3650b4ffad31d191f7f1193ee387cc8
sherifm <sherifmostafa92@googlemail.com>
2015-12-10 09:32:56 -0600

    Added pr2 web animation

--- src/create_csv.py
+++ src/create_csv.py
@@ -3,25 +3,67 @@ import rospy
 from math import sin, cos, pi
 import numpy as np
 
-
-#Kuka youbot joints
+#PR2 joints
 joint_names = [
-   "arm_joint_1",
-   "arm_joint_2",
-   "arm_joint_3",
-   "arm_joint_4",
-   "arm_joint_5",
-   "caster_joint_fl",
-   "wheel_joint_fl",
-   "caster_joint_fr",
-   "wheel_joint_fr",
-   "caster_joint_bl",
-   "wheel_joint_bl",
-   "caster_joint_br",
-   "wheel_joint_br",
-   "gripper_finger_joint_l",
-   "gripper_finger_joint_r",
+"fl_caster_rotation_joint",
+"fl_caster_l_wheel_joint",
+"fl_caster_r_wheel_joint",
+"fr_caster_rotation_joint",
+"fr_caster_l_wheel_joint",
+"fr_caster_r_wheel_joint",
+"bl_caster_rotation_joint",
+"bl_caster_l_wheel_joint",
+"bl_caster_r_wheel_joint",
+"br_caster_rotation_joint",
+"br_caster_l_wheel_joint",
+"br_caster_r_wheel_joint",
+"torso_lift_joint",
+"torso_lift_motor_screw_joint",
+"head_pan_joint",
+"head_tilt_joint",
+"laser_tilt_mount_joint",
+"r_shoulder_pan_joint",
+"r_shoulder_lift_joint",
+"r_upper_arm_roll_joint",
+"r_forearm_roll_joint",
+"r_elbow_flex_joint",
+"r_wrist_flex_joint",
+"r_wrist_roll_joint",
+"r_gripper_motor_slider_joint",
+"r_gripper_motor_screw_joint",
+"r_gripper_l_finger_joint",
+"r_gripper_joint",
+"l_shoulder_pan_joint",
+"l_shoulder_lift_joint",
+"l_upper_arm_roll_joint",
+"l_forearm_roll_joint",
+"l_elbow_flex_joint",
+"l_wrist_flex_joint",
+"l_wrist_roll_joint",
+"l_gripper_motor_slider_joint",
+"l_gripper_motor_screw_joint",
+"l_gripper_l_finger_joint",
+"l_gripper_joint",
 ]
+
+# #Kuka youbot joints
+# joint_names = [
+#    "arm_joint_1",
+#    "arm_joint_2",
+#    "arm_joint_3",
+#    "arm_joint_4",
+#    "arm_joint_5",
+#    "caster_joint_fl",
+#    "wheel_joint_fl",
+#    "caster_joint_fr",
+#    "wheel_joint_fr",
+#    "caster_joint_bl",
+#    "wheel_joint_bl",
+#    "caster_joint_br",
+#    "wheel_joint_br",
+#    "gripper_finger_joint_l",
+#    "gripper_finger_joint_r",
+# ]
 
 #Barret wam7 joints
 #joint_names = [
@@ -54,10 +96,16 @@ joint_names = [
 DT = 0.1
 TF = 10.0
 
-j1func = lambda t: 0.75*sin(t) + 2.95
-j2func = lambda t: 0.3*sin(2*t) + 1.35
-j3func = lambda t: 0.2*sin(2*t) - 2.59
-j4func = lambda t: 0.4*sin(t)
+# j1func = lambda t: 0.75*sin(t) + 2.95
+# j2func = lambda t: 0.3*sin(2*t) + 1.35
+# j3func = lambda t: 0.2*sin(2*t) - 2.59
+# j4func = lambda t: 0.4*sin(t)
+
+#PR2 Funtions for torso_lift, head_tilt, r&l elbow flex
+j1func = lambda t: 0.2*sin(0.5*t) + 0.16
+j2func = lambda t: 0.8*sin(2*t) + 0.45
+j3func = lambda t: 0.8*sin(t) - 1
+j4func = lambda t: -0.8*sin(t) - 1
 
 tvec = np.arange(0,TF+DT,DT)
 dat = np.zeros((len(tvec), len(joint_names)+1))
@@ -65,10 +113,10 @@ dat = np.zeros((len(tvec), len(joint_names)+1))
 for i,t in enumerate(tvec):
     tmpdat = np.zeros(len(dat[i]))
     tmpdat[0] = t
-    tmpdat[1] = j1func(t)
-    tmpdat[2] = j2func(t)
-    tmpdat[3] = j3func(t)
-    tmpdat[4] = j4func(t)
+    tmpdat[13] = j1func(t)
+    tmpdat[16] = j2func(t)
+    tmpdat[22] = j3func(t)
+    tmpdat[33] = j4func(t)
     dat[i] = tmpdat
 
 # fd = open("/home/jarvis/me495_indigows/src/kinematics_animation/data/jointstates.csv",'w')
